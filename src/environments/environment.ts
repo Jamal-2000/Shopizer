c5957e6be894799d3bb2f850b5a378f50a95c015
Jamal-2000 <139530206+Jamal-2000@users.noreply.github.com>
2023-07-20 15:52:37 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -17,8 +17,8 @@ export const environment = {
     //MARKETPLACE | BTB | STANDARD
     mode: 'STANDARD',
     //API URL
-    apiUrl: "http://34.131.11.128:8080/api",
-    shippingApi: 'http://34.131.11.128:9090/shipping/api/v1',
+    apiUrl: "http://34.30.90.215:8080/api",
+    shippingApi: 'http://34.30.90.215:9090/shipping/api/v1',
     client: {
         language: {
             default: 'fr',
