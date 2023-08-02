038c57174e564e055d79904a3db0d80d71e0e58a
Jamal-2000 <139530206+Jamal-2000@users.noreply.github.com>
2023-08-03 00:04:14 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,8 +8,8 @@ export const environment = {
     //MARKETPLACE | BTB | STANDARD
     mode: 'STANDARD',
     //API URL
-    apiUrl: window["env"]["APP_BASE_URL"] || 'https://34.30.39.1:4200/api/v1',
-    shippingApi: window["env"]["APP_SHIPPING_URL"] || 'http://34.30.39.171:4200/api',
+    apiUrl: window["env"]["APP_BASE_URL"] || 'http://34.131.214.114:8080/api/v1',
+    shippingApi: window["env"]["APP_SHIPPING_URL"] || 'http://34.131.214.114:9090/api',
     client: {
         language: {
             default: window["env"]["APP_DEFAULT_LANGUAGE"] || 'fr',
